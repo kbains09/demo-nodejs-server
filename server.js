1c594c3d96e8200b3dda4e1cf8a2597616516986
Kirat Bains <kiratbains09@gmail.com>
2023-09-12 00:54:41 -0700

    feat: tick tok

--- server.js
+++ server.js
@@ -1,6 +1,50 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const app = express();
-const port = process.env.PORT || 3000; // Define the port for your server.
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Connect to MongoDB (make sure MongoDB is running)
+mongoose.connect('mongodb://localhost/task_management', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error: ' + err);
+});
+
+// Create a Task model
+const Task = mongoose.model('Task', {
+  title: String,
+  description: String,
+  completed: Boolean,
+});
+
+// Middleware
+app.use(bodyParser.json());
+
+// API routes
+app.get('/api/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/api/tasks', async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(400).json({ error: 'Bad Request' });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
